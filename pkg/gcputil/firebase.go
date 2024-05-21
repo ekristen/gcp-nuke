b5ef0d6530e0ee6ff770fbf2242470f21db6cfd5
Erik Kristensen <erik@erikkristensen.com>
2024-05-21 16:55:04 -0600

    fix: golangci-lint violations

--- pkg/gcputil/firebase.go
+++ pkg/gcputil/firebase.go
@@ -1,25 +1,20 @@
 package gcputil
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/api/option"
 	"google.golang.org/api/option/internaloption"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"time"
-
 	htransport "google.golang.org/api/transport/http"
+	"io"
+	"net/http"
 )
 
-// FirebaseDBClient is a client to interact with the Firebase Realtime Database API
-type FirebaseDBClient struct {
-	httpClient *http.Client
-}
+const basePath = "https://firebasedatabase.googleapis.com/"
+const basePathTemplate = "https://firebasedatabase.UNIVERSE_DOMAIN/"
+const mtlsBasePath = "https://firebasedatabase.mtls.googleapis.com/"
 
 // DatabaseInstance represents a Firebase Realtime Database instance
 type DatabaseInstance struct {
@@ -29,63 +24,6 @@ type DatabaseInstance struct {
 	Type        string `json:"type"`
 	State       string `json:"state"`
 }
-
-type loggingTransport struct {
-	transport http.RoundTripper
-}
-
-func (t *loggingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	start := time.Now()
-
-	// Log the request
-	logRequest(req)
-
-	// Perform the request
-	res, err := t.transport.RoundTrip(req)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return nil, err
-	}
-
-	// Log the response
-	logResponse(res, time.Since(start))
-
-	return res, nil
-}
-
-func logRequest(req *http.Request) {
-	fmt.Printf("Request: %s %s\n", req.Method, req.URL)
-	fmt.Printf("Headers: %v\n", req.Header)
-
-	if req.Body != nil {
-		bodyBytes, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			fmt.Printf("Error reading request body: %v\n", err)
-		} else {
-			req.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes)) // Reassign the body
-			fmt.Printf("Body: %s\n", string(bodyBytes))
-		}
-	}
-}
-
-func logResponse(res *http.Response, duration time.Duration) {
-	fmt.Printf("Response: %s in %v\n", res.Status, duration)
-	fmt.Printf("Headers: %v\n", res.Header)
-
-	if res.Body != nil {
-		bodyBytes, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			fmt.Printf("Error reading response body: %v\n", err)
-		} else {
-			res.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes)) // Reassign the body
-			fmt.Printf("Body: %s\n", string(bodyBytes))
-		}
-	}
-}
-
-const basePath = "https://firebasedatabase.googleapis.com/"
-const basePathTemplate = "https://firebasedatabase.UNIVERSE_DOMAIN/"
-const mtlsBasePath = "https://firebasedatabase.mtls.googleapis.com/"
 
 // NewFirebaseDatabaseService creates a new Firebase Realtime Database client to interact with the
 // https://firebasedatabase.googleapis.com endpoints as there is no official golang client library
@@ -98,7 +36,6 @@ func NewFirebaseDatabaseService(ctx context.Context, opts ...option.ClientOption
 	)
 	// NOTE: prepend, so we don't override user-specified scopes.
 	opts = append([]option.ClientOption{scopesOption}, opts...)
-	opts = append(opts, internaloption.WithDefaultEndpoint(basePath))
 	opts = append(opts, internaloption.WithDefaultEndpointTemplate(basePathTemplate))
 	opts = append(opts, internaloption.WithDefaultMTLSEndpoint(mtlsBasePath))
 	opts = append(opts, internaloption.EnableNewAuthLibrary())
