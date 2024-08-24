165292aa45f5fa92184ff6f6ae5278f00e3b9a37
Erik Kristensen <erik@erikkristensen.com>
2024-08-24 14:42:48 -0600

    feat(bigquery-dataset): new resource

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ekristen/gcp-nuke
 go 1.22
 
 require (
+	cloud.google.com/go/bigquery v1.62.0
 	cloud.google.com/go/compute v1.28.0
 	cloud.google.com/go/container v1.39.0
 	cloud.google.com/go/functions v1.19.0
@@ -32,22 +33,29 @@ require (
 	cloud.google.com/go/compute/metadata v0.5.0 // indirect
 	cloud.google.com/go/firestore v1.16.0 // indirect
 	cloud.google.com/go/longrunning v0.5.12 // indirect
+	github.com/apache/arrow/go/v15 v15.0.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/flatbuffers v23.5.26+incompatible // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mb0/glob v0.0.0-20160210091149-1eb79d2de6c4 // indirect
+	github.com/pierrec/lz4/v4 v4.1.18 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stevenle/topsort v0.2.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.52.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
@@ -55,10 +63,14 @@ require (
 	go.opentelemetry.io/otel/metric v1.28.0 // indirect
 	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/mod v0.18.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
---
+++ resources/bigquery-dataset.go
@@ -0,0 +1,128 @@
+package resources
+
+import (
+	"context"
+	"errors"
+	"github.com/gotidy/ptr"
+	"github.com/sirupsen/logrus"
+
+	"google.golang.org/api/iterator"
+
+	"cloud.google.com/go/bigquery"
+
+	"github.com/ekristen/libnuke/pkg/registry"
+	"github.com/ekristen/libnuke/pkg/resource"
+	"github.com/ekristen/libnuke/pkg/types"
+
+	"github.com/ekristen/gcp-nuke/pkg/nuke"
+)
+
+const BigQueryDatasetResource = "BigQueryDataset"
+
+func init() {
+	registry.Register(&registry.Registration{
+		Name:   BigQueryDatasetResource,
+		Scope:  nuke.Project,
+		Lister: &BigQueryDatasetLister{},
+	})
+}
+
+type BigQueryDatasetLister struct {
+	svc *bigquery.Client
+}
+
+func (l *BigQueryDatasetLister) List(ctx context.Context, o interface{}) ([]resource.Resource, error) {
+	opts := o.(*nuke.ListerOpts)
+	var resources []resource.Resource
+	if err := opts.BeforeList(nuke.Regional, "bigquery.googleapis.com"); err != nil {
+		return resources, err
+	}
+
+	if l.svc == nil {
+		var err error
+		l.svc, err = bigquery.NewClient(ctx, *opts.Project, opts.ClientOptions...)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// NOTE: you might have to modify the code below to actually work, this currently does not
+	// inspect the google go sdk instead is a jumping off point
+	it := l.svc.Datasets(ctx)
+	for {
+		resp, err := it.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+		if err != nil {
+			logrus.WithError(err).Error("unable to iterate networks")
+			break
+		}
+
+		meta, err := resp.Metadata(ctx)
+		if err != nil {
+			logrus.WithError(err).Error("unable to get dataset metadata")
+			continue
+		}
+
+		if meta.Location != ptr.ToString(opts.Region) {
+			continue
+		}
+
+		resources = append(resources, &BigQueryDataset{
+			svc:      l.svc,
+			dataset:  resp,
+			Name:     ptr.String(meta.Name),
+			Location: ptr.String(meta.Location),
+			Labels:   meta.Labels,
+		})
+	}
+
+	return resources, nil
+}
+
+type BigQueryDataset struct {
+	svc      *bigquery.Client
+	project  *string
+	region   *string
+	dataset  *bigquery.Dataset
+	Name     *string
+	Location *string
+	Labels   map[string]string `property:"tagPrefix=label"`
+}
+
+func (r *BigQueryDataset) Remove(ctx context.Context) error {
+	if err := r.removeTables(ctx); err != nil {
+		return err
+	}
+
+	return r.dataset.Delete(ctx)
+}
+
+func (r *BigQueryDataset) removeTables(ctx context.Context) error {
+	it := r.dataset.Tables(ctx)
+	for {
+		resp, err := it.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+		if err != nil {
+			logrus.WithError(err).Error("unable to iterate tables")
+			break
+		}
+
+		if err := resp.Delete(ctx); err != nil {
+			logrus.WithError(err).Error("unable to delete table")
+		}
+	}
+
+	return nil
+}
+
+func (r *BigQueryDataset) Properties() types.Properties {
+	return types.NewPropertiesFromStruct(r)
+}
+
+func (r *BigQueryDataset) String() string {
+	return *r.Name
+}
